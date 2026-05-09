af71a8b8e1d7da8fe0218025a838c389e31a8b06
Samgu Lee <cable8mm@gmail.com>
2026-05-09 21:41:33 +0900

    Renew tests

--- tests/SensSmsChannelTest.php
+++ tests/SensSmsChannelTest.php
@@ -2,31 +2,71 @@
 
 namespace Seungmun\Sens\Tests;
 
-use GuzzleHttp\Client;
+use Illuminate\Notifications\Notification;
 use Mockery as m;
 use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
+use Seungmun\Sens\Sms\Sms;
+use Seungmun\Sens\Sms\SmsChannel;
+use Seungmun\Sens\Sms\SmsMessage;
 
 class SensSmsChannelTest extends TestCase
 {
     /**
-     * @var MockInterface|Client
+     * @var MockInterface|Sms
      */
-    private $guzzleHttp;
+    private $sms;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->guzzleHttp = m::mock(Client::class);
+        $this->sms = m::mock(Sms::class);
     }
 
-    /**
-     * @return void
-     */
-    public function test_iam_sorry_because_tests_are_so_nuisance()
+    public function test_it_sends_the_sms_notification_message(): void
     {
-        $this->assertTrue(true);
+        $payload = [
+            'type' => 'SMS',
+            'contentType' => 'COMM',
+            'countryCode' => '82',
+            'from' => '0550000000',
+            'subject' => null,
+            'content' => 'Your purchase receipt is ready.',
+            'messages' => [
+                ['to' => '01012345678'],
+            ],
+        ];
+
+        $message = m::mock(SmsMessage::class);
+        $message->shouldReceive('toArray')
+            ->once()
+            ->andReturn($payload);
+
+        $notifiable = new class
+        {
+            public string $phone = '01012345678';
+        };
+
+        $notification = new class($message) extends Notification
+        {
+            public function __construct(private SmsMessage $message)
+            {
+            }
+
+            public function toSms($notifiable): SmsMessage
+            {
+                return $this->message;
+            }
+        };
+
+        $this->sms->shouldReceive('send')
+            ->once()
+            ->with($payload);
+
+        (new SmsChannel($this->sms))->send($notifiable, $notification);
+
+        $this->addToAssertionCount(1);
     }
 
     protected function tearDown(): void
